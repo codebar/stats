8d80f0265d8f0b8941c3f042af7e5a5e0d781d30
Alexander Keliris <rigellute@gmail.com>
2020-05-12 15:49:45 +0100

    Use inter font

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
   purge: ["./src/**/*.js"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
