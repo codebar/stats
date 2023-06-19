dd973998e79103b6266f31c953d7faea06ec15e2
KimberleyCook <KimberleyCook@users.noreply.github.com>
2023-06-19 16:29:49 +0000

    deploy: 6ecdf7348c6f2dd94f8c697ecce1acd76cc9718d

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4d9b3ef2feb666530390.js"
+    "url": "webpack-runtime-18ffe08b2b4ab106a16a.js"
   },
   {
     "url": "framework-be38b7081e4d62e4794d.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1a1cba2f30219f3d78be897e66d11349"
+    "revision": "2954b3c731c85cc17328acb080f1c216"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-aac1c5c58ecb0eb6f223.js"
