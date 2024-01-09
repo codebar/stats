e1b3e3a038671380c004eaf1e33eed946e876be6
Kimberley Cook <kimberleycook@Kimberleys-MacBook-Air.local>
2024-01-09 12:42:57 +0100

    update eslint package

--- www/src/untyped.d.ts
+++ www/src/untyped.d.ts
@@ -2,6 +2,9 @@ declare module "chart.js";
 declare module "react-chartkick";
 declare module "tailwindcss/defaultTheme";
 
+declare module "*.png";
+declare module "*.jpg";
+
 declare module "*.svg" {
   const content: string;
   export default content;
